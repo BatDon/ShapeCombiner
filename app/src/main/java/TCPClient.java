4be14c6b3c7617d21fda3703c2f773af4ac321e6
BatDon <asics007@gmail.com>
2018-09-17 17:18:26 -0500

    Initial commit

---
+++ app/src/main/java/TCPClient.java
@@ -0,0 +1,8 @@
+private static String TAG = "TCPClient"; //For debugging, always a good idea to have defined
+private String severIp ="192.168.0.2";
+private long startTime = 0l;
+private int serverPort = 1234;
+private Socket connectionSocket;
+
+public class TCPClient {
+}
